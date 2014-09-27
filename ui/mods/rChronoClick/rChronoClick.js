7c00a7c799fef3e85bc8fcdf4889e0d5916cf9c5
Raevn <d_bened@raevn.com>
2014-09-27 13:07:51 +1000

    v1.0.1
    Support for new panel system

--- ui/mods/rChronoClick/rChronoClick.js
+++ ui/mods/rChronoClick/rChronoClick.js
@@ -1,7 +1,16 @@
 model.scrubToTime = function () {
 	var event = window.event;
-	var time = ((event.screenX - $('.div_time_progress_frame').offset().left) / 384) * model.endOfTimeInSeconds();
-	api.time.set(Number(time));
+	
+	if (event.screenX >= $('.div_time_progress_frame').offset().left) {
+		if (event.screenX <= $('.div_time_progress_frame').offset().left + $('.div_time_progress_frame').width()) {
+			if (event.screenY >= $('.div_time_progress_frame').offset().top) {
+				if (event.screenY <= $('.div_time_progress_frame').offset().top + $('.div_time_progress_frame').height()) {
+					var time = ((event.screenX - $('.div_time_progress_frame').offset().left) / $('.div_time_progress_frame').width()) * model.endOfTimeInSeconds();
+					api.time.set(Number(time));
+				}
+			}
+		}
+	}
 }
 
-$('.div_time_progress_frame').mousedown(function() { model.scrubToTime()});
+$('.body_panel').mousedown(function() { model.scrubToTime()});
